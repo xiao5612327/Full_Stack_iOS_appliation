b56e6e4c5b8595fef1ab8994c34d6c8d8331b185
Xiaoping Weng <xiaopingweng@Xiaopings-MacBook-Pro.local>
2019-12-05 01:30:40 -0800

    set up web app, and connect app to mongoDB Atlas

---
+++ web_social/config/session.js
@@ -0,0 +1,42 @@
+/**
+ * Session Configuration
+ * (sails.config.session)
+ *
+ * Use the settings below to configure session integration in your app.
+ * (for additional recommended settings, see `config/env/production.js`)
+ *
+ * For all available options, see:
+ * https://sailsjs.com/config/session
+ */
+
+module.exports.session = {
+
+  /***************************************************************************
+  *                                                                          *
+  * Session secret is automatically generated when your new app is created   *
+  * Replace at your own risk in production-- you will invalidate the cookies *
+  * of your users, forcing them to log in again.                             *
+  *                                                                          *
+  ***************************************************************************/
+  secret: '39627c5380d51129bd8b608a36f5b393',
+
+  adapter: 'connect-mongo',
+  url: 'mongodb://socialAdmin:xiao5612327@cluster0-shard-00-00-kwnal.mongodb.net:27017,cluster0-shard-00-01-kwnal.mongodb.net:27017,cluster0-shard-00-02-kwnal.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  collections: 'sessions',
+
+  /***************************************************************************
+  *                                                                          *
+  * Customize when built-in session support will be skipped.                 *
+  *                                                                          *
+  * (Useful for performance tuning; particularly to avoid wasting cycles on  *
+  * session management when responding to simple requests for static assets, *
+  * like images or stylesheets.)                                             *
+  *                                                                          *
+  * https://sailsjs.com/config/session                                       *
+  *                                                                          *
+  ***************************************************************************/
+  // isSessionDisabled: function (req){
+  //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
+  // },
+
+};
